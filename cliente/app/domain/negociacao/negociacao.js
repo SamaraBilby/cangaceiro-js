abd25cb4e726253d829fbfd3097ed5198d6d5c4f
SamaraBilby <samarabilby@hotmail.com>
2023-02-16 14:22:46 -0300

    Usando Object.freeze, get e set

--- cliente/app/domain/negociacao/negociacao.js
+++ cliente/app/domain/negociacao/negociacao.js
@@ -12,29 +12,43 @@ class Negociacao {
         
         /* Através de this.nomeDaPropriedade, especificamos que a negociação terá: data, quantidade e valor, cada propriedade com seu valor padrão   */
 
-        this._data = data
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
-        this._valor = valor; 
+        this._valor = valor;
+        Object.freeze(this)
     }
 
     /* Funções que definem um comportamento de uma classe são chamadas de métodos para alinhar o vocabulário com o paradigma da Orientação a Objetos. */
     
-    obtemVolume() {
+    //getVolume
+        get volume() {
 
         /* as propriedades que contenham _ (underline) só poderão ser acessadas pelos próprios métodos da classe. Isto significa que, mesmo podendo imprimir a propriedade _quantidade com outro valor, não deveríamos mais poder acessá-la. O _ funciona como um aviso dizendo: "programador, você não pode alterar esta propriedade!". */
 
         return this._quantidade * this._valor;
     }
-
-    getData(){
+    //Métodos acessadores geralmente começam com o prefixo get em seus nomes.
+    
+    /*Como são métodos da própria classe, seguindo nossa convenção, 
+    eles podem acessar às propriedades prefixadas com (_) . 
+    E qualquer outro código que precisar ler as propriedades de um 
+    objeto do tipo Negociacao poderá fazê-lo através dos
+    métodos acessadores que criamos.*/
+    
+    /* Para enxugar nossos métodos acessadores através da sintaxe get 
+    que a linguagem nos oferece.*/
+    
+    //getData
+        get data(){
         return this._data;
     }
 
-    getQuantidade(){
+    //getQuantidade
+        get quantidade(){
         return this._quantidade;
     }
-
-    getValor(){
+    // getValor
+    get valor(){
         return this._valor;
     }
 }
