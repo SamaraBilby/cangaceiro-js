22505676ef6be772e71147db34f095129f906d68
SamaraBilby <samarabilby@hotmail.com>
2023-02-22 16:25:00 -0300

    utilizando bind

--- cliente/app/controllers/NegociacaoController.js
+++ cliente/app/controllers/NegociacaoController.js
@@ -4,6 +4,15 @@ class NegociacaoController{
         // cancelando a submissão do formulário
         event.preventDefault();
 
-        alert("chamei ação no controller")
+        // realizando o bind, $ mantém document com seu contexto this.
+        let $ = document.querySelector.bind(document);
+        
+        let inputData = $('#data');
+        let inputQuantidade = $('#quantidade');
+        let inputValor = $('#valor');
+
+        console.log(typeof (inputData.value))
+        console.log(parseInt(inputQuantidade.value))
+        console.log(parseFloat(inputValor.value))
     }
 }
