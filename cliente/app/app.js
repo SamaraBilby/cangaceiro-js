eb76eecd6760de59146b27c9b982a4008c44decb
SamaraBilby <samarabilby@hotmail.com>
2023-02-23 16:01:59 -0300

    spread operator ...

--- cliente/app/app.js
+++ cliente/app/app.js
@@ -5,4 +5,4 @@ let controller =  new NegociacaoController();
 
 document
         .querySelector('.form')
-        .addEventListener('submit', controller.adiciona);
+        .addEventListener('submit', controller.adiciona.bind(controller));
--- cliente/app/controllers/NegociacaoController.js
+++ cliente/app/controllers/NegociacaoController.js
@@ -1,18 +1,30 @@
 class NegociacaoController{
-    adiciona(event){
-         
-        // cancelando a submissão do formulário
-        event.preventDefault();
+
+    constructor(){
 
         // realizando o bind, $ mantém document com seu contexto this.
         let $ = document.querySelector.bind(document);
-        
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
+            
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+    adiciona(event){
 
-        console.log(typeof (inputData.value))
-        console.log(parseInt(inputQuantidade.value))
-        console.log(parseFloat(inputValor.value))
+         // cancelando a submissão do formulário
+        event.preventDefault();
+        
+        // o spread operator (...), que permite tratar cada elemento do array como um elemento individualmente
+        let data = new Date(...this._inputData.value.split('-').map(function(item){
+            return item;})
+            );
+        
+            console.log(data)
+
+        let negociacao = new Negociacao(
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        )
+
     }
 }
