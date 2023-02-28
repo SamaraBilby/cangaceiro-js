b87ae61c09ad6b66b1f2ad2ece42162cdbbdf852
SamaraBilby <samarabilby@hotmail.com>
2023-02-28 14:30:48 -0300

    usando o modificador static para não instanciar a class

--- cliente/app/controllers/NegociacaoController.js
+++ cliente/app/controllers/NegociacaoController.js
@@ -14,16 +14,14 @@ class NegociacaoController{
          // cancelando a submissão do formulário
         event.preventDefault();
         
-        let converter = new DateConverter();
-        let data = converter.paraData(this._inputData.value)
 
         let negociacao = new Negociacao(
-            data,
+            DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-
-        let diaMesAno = converter.paraTexto(negociacao.data);
+            console.log(negociacao.data)
+        let diaMesAno = DateConverter.paraTexto(negociacao.data);
         console.log(diaMesAno);
     }
 }
--- cliente/app/ui/converters/DateConverter.js
+++ cliente/app/ui/converters/DateConverter.js
@@ -1,9 +1,22 @@
 class DateConverter {
 
-    paraTexto(data) {
+    // construtor para avisar ao programador que não precisa instanciar a classe
+
+    constructor() {
+        throw new Error('Essa classe não pode ser instanciada');
+    }
+  /*
+  Quando temos métodos que não fazem sentido serem
+  chamados de uma instância, como no caso do método
+  paraTexto() que criamos, podemos chamá-los diretamente da
+  classe na qual foram declarados, adicionando o modificador
+  static ( vira algo como "uma variável global da classe") antes do nome do método: 
+  */
+
+    static paraTexto(data) {
         return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`
     }
-     paraData(texto) {
+    static paraData(texto) {
         // o spread operator (...), que permite tratar cada elemento do array como um elemento individualmente
         // o map() atuará em cada elemento do array, retornando um novo array no final
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
